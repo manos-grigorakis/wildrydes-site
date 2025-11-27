ff6565a28f6faa60c35bfff3796f309102d1b615
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-27 18:47:16 +0200

    Update AWS Cognito configuration in config.js

--- js/config.js
+++ js/config.js
@@ -1,10 +1,11 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_AzGCT1pw1', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '2k88lgeje1stq67bi49ju643b7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
