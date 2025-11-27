28554259422a27be1667d0a6c3c0622ba2d8c479
Manos Grigorakis <manos.grigorakis03@gmail.com>
2025-11-27 18:53:54 +0200

    Update userPoolClientId in config.js

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
         userPoolId: 'eu-central-1_AzGCT1pw1', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '2k88lgeje1stq67bi49ju643b7', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolClientId: '5nlngk9aaq7ncqava1k206un7b', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'eu-central-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
